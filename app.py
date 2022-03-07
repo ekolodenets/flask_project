d3b344ae9cd05a2d107915a9e49d6fadebca681a
ekolodenets <66313736+ekolodenets@users.noreply.github.com>
2022-03-07 13:17:59 +0300

    update

--- app.py
+++ app.py
@@ -631,5 +631,5 @@ class Users(db.Model, UserMixin):
 
 '''END MODELS'''
 
-if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="localhost", port=5000, debug=True)
